6c2a582d16ce747d6cef160e1ef3d45ecff38048
yuma1002 <yuma111002@gmail.com>
2022-10-09 13:54:56 +0000

    [Start]

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root :to => 'homes#top'
+  get 'books/new'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  post 'books' => 'books#create'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
